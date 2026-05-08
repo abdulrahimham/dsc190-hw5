87da5259984bb0a41d779bc8776affd475bc6747
abdulrahimham <amham@ucsd.edu>
2026-05-08 03:59:53 +0000

    Fix ruff violations

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import sys
-import os
-
-x = None
-if x == None:
-    print("x is None")
-
-unused_variable = "never used"
-
 def greet(name):
-    greeting = "Hello, " + name + "! Welcome to DSC 190, Tools of the Trade, Spring 2026."
+    greeting = f"Hello, {name}!"
     return greeting
+
 
 print(greet("World"))
