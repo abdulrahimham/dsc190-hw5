a19dcda42bd9c049d34ba11340cf33311c2cfdbe
abdulrahimham <amham@ucsd.edu>
2026-05-08 03:56:49 +0000

    Introduce ruff violations

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from dsc190-hw5!")
+import sys
+import os
 
+x = None
+if x == None:
+    print("x is None")
 
-if __name__ == "__main__":
-    main()
+unused_variable = "never used"
+
+def greet(name):
+    greeting = "Hello, " + name + "! Welcome to DSC 190, Tools of the Trade, Spring 2026."
+    return greeting
+
+print(greet("World"))
